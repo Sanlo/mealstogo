391a40a4aab108dd8853b51685288db4904451af
Sanlo <940072028@qq.com>
2024-03-03 15:31:47 +0800

    add basic layout and ESLint support

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, StatusBar, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>Search</Text>
+        </View>
+
+        <View style={styles.mealList}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: StatusBar.currentHeight,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  mealList: {
+    flex: 1,
+    padding: 16,
+    flexGrow: 1,
+    backgroundColor: "blue",
   },
 });
