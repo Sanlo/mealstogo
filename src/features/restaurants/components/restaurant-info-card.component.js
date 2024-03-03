0f08b23490159289299162d4407a614f9a6851fb
Sanlo <940072028@qq.com>
2024-03-03 22:37:47 +0800

    add expo google font to style

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -14,7 +14,7 @@ const RestaurantCardCover = styled(Card.Cover)`
 
 const Title = styled(Text)`
   padding: ${(props) => props.theme.space[3]};
-  font-size: ${(props) => props.theme.fontSizes.title};
+  font-family: ${(props) => props.theme.fonts.body};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
 
