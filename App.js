42159883827752d640ff490cebf20a7560d6d353
Sanlo <940072028@qq.com>
2024-03-03 17:18:34 +0800

    add restaurant info card component

--- App.js
+++ App.js
@@ -1,40 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StyleSheet, SafeAreaView, StatusBar, Text, View } from "react-native";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Text>Search</Text>
-        </View>
-
-        <View style={styles.mealList}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    marginTop: StatusBar.currentHeight,
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-  mealList: {
-    flex: 1,
-    padding: 16,
-    flexGrow: 1,
-    backgroundColor: "blue",
-  },
-});
---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,37 @@
+﻿import React, { useState } from "react";
+import { Searchbar } from "react-native-paper";
+import { StyleSheet, SafeAreaView, StatusBar, Text, View } from "react-native";
+
+import { RestaurantsInfoCard } from "../components/restaurant-info-card.component";
+
+export const RestaurantsScreen = () => {
+  const [query, setQuery] = useState("");
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar placeholder="Search" onChangeText={setQuery} value={query} />
+      </View>
+
+      <View style={styles.list}>
+        <RestaurantsInfoCard />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    flexGrow: 1,
+    backgroundColor: "blue",
+  },
+});
