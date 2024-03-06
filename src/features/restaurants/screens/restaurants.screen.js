74073f38263616cfce8512b0a3cc87ba33f25540
Sanlo <sanlozhang@skiff.com>
2024-03-06 10:11:51 +0800

    add loading indicator feature

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,12 +1,22 @@
 ﻿import React, { useState, useContext } from "react";
 import styled from "styled-components";
-import { Searchbar } from "react-native-paper";
+import { ActivityIndicator, MD2Colors, Searchbar } from "react-native-paper";
 import { FlatList, View } from "react-native";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { RestaurantsInfoCard } from "../components/restaurant-info-card.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
 
 const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]};
@@ -20,6 +30,12 @@ export const RestaurantsScreen = () => {
 
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={MD2Colors.red800} />
+        </LoadingContainer>
+      )}
+
       <SearchContainer>
         <Searchbar placeholder="Search" onChangeText={setQuery} value={query} />
       </SearchContainer>
