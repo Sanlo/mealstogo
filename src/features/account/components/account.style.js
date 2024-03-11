83a4deaa90f8f42ea07ea54858abe4cbf124b752
Sanlo <sanlozhang@skiff.com>
2024-03-11 22:41:42 +0800

    add background image component

---
+++ src/features/account/components/account.style.js
@@ -0,0 +1,13 @@
+﻿import styled from "styled-components/native";
+
+export const BackgroundWrapper = styled.View`
+  flex: 1;
+`;
+
+export const BackgroundImage = styled.ImageBackground.attrs({
+  source: require("../../../../assets/home_bg.jpg"),
+})`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/features/account/components/background.component.js
@@ -0,0 +1,9 @@
+﻿import React from "react";
+
+import { BackgroundImage, BackgroundWrapper } from "./account.style";
+
+export const AccountBackground = () => (
+  <BackgroundWrapper>
+    <BackgroundImage></BackgroundImage>
+  </BackgroundWrapper>
+);
